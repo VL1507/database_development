c65f7ca68c6a40f78921ec7806a8e8332784d38e
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-03-20 15:24:56 +1000

    ok

--- 02I Обновление значений поля/02I_solution.sql
+++ 02I Обновление значений поля/02I_solution.sql
@@ -1,1 +1,11 @@
-
+UPDATE Animals
+SET Sex = "unknown"
+WHERE Sex IS NOT NULL
+    AND Sex != "male"
+    AND Sex != "female";
+UPDATE Animals
+SET Sex = "m"
+WHERE Sex = "male";
+UPDATE Animals
+SET Sex = "w"
+WHERE Sex = "female";
