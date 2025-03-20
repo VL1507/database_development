66fffbf1a273e2292aadb4870fd9fb5c5bbc7e8e
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-03-20 15:42:16 +1000

    ok

--- 02A Создание таблицы с заметками/02A_solution.sql
+++ 02A Создание таблицы с заметками/02A_solution.sql
@@ -1,1 +1,14 @@
+-- database: .\02A.db
+DROP TABLE Notes;
 
+CREATE TABLE
+    Notes (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Note TEXT UNIQUE NOT NULL,
+        TimeOfCreation DATETIME NOT NULL,
+        ProgressMade REAL CHECK (
+            ProgressMade >= 0
+            AND ProgressMade <= 1
+        ) DEFAULT 0,
+        Status TEXT CHECK (Status IN ('started', 'accepted', 'canceled')) DEFAULT 'started'
+    )
